308f3d75d5fd919393ef558224876f7914289051
Gustavo Quiroga <gtavo.quiroga@gmail.com>
2024-03-12 19:22:04 -0300

    correccion

--- script.js
+++ script.js
@@ -15,51 +15,55 @@ function usuario(id , nombre , apellido , saldo , puntos , password ,dni , tarje
 //Creacion de usuarios
 const usuario1 = new usuario(1 ,"gustavo", "quiroga",25000,650,2236,33106354,["visa debito" , "visa credito" ,"tarjeta de puntos"])
 const usuario2 = new usuario(2 , "cecilia" ,"osiglio",39000,0,1234,32908422,["visa debito" , "visa credito" , "mastercard credito"])
-console.log(usuario1)
+
+
 
 
 //Menu Login
 
-let online = false
 let submit = document.getElementById("submit");
 submit.onclick=logear
 
 function logear(){
 
+fetch(`usuarios.json`)
+.then(respuesta=>respuesta.json())
+.then(data=>{
+    localStorage.setItem("users",JSON.stringify(data))
+})  
+    
+let users = JSON.parse(localStorage.getItem(`users`))
+console.log(users);
 let loginNombre = document.getElementById("login-nombre").value;
 let loginPassword = document.getElementById("login-password").value;
 
 
+if(usuarioOnline=users.find(user=>user.nombre===loginNombre && user.password===parseInt(loginPassword))){
 
-
-if (loginNombre==usuario1.nombre && loginPassword==usuario1.password) {
-    online=true
-
-    localStorage.setItem(`usuario1` , JSON.stringify(usuario1))
-    let usuario1LS = JSON.parse(localStorage.getItem("usuario1"))
+console.log(usuarioOnline);
 
     let contenido = document.querySelector(".contenido").innerHTML=`<div class="contenido-active">
-    <h2>Bienvenido ${usuario1LS.nombre}</h2>
+    <h2>Bienvenido ${usuarioOnline.nombre}</h2>
     <br>
-    <h2>Tu salgo es de $${usuario1LS.saldo}</h2>
+    <h2>Tu salgo es de $${usuarioOnline.saldo}</h2>
     <br>
     </div>`
 
 
     let operaciones = document.querySelector(".operaciones");
-    operaciones.style.display="flex"   
+        operaciones.style.display="flex"   
 
 }
 else{
 
+
 let resultado = document.querySelector(".resultado")
-
-resultado.className= "my-3 p-3 border border-danger text-danger";
-resultado.innerHTML="El usuario ingresado no existe"
-}
-
+resultado.innerHTML=`<p class="my-3 p-3 border border-danger text-danger">El usuario ingresado no existe</p>`
 
 }
+
+}
+
 //Menu app 
 
 //Banco digital
@@ -84,18 +88,25 @@ consultar.onclick=checkearDatosUsuario
 
 //funciones
 
-function ingresarDinero(){
+function ingresarDinero(){ 
+
+    let contenido = document.querySelector(".contenido").innerHTML=`<div class="contenido-active">
+    <h2>Bienvenido ${usuarioOnline.nombre}</h2>
+    <br>
+    <h2>Tu salgo es de $${usuarioOnline.saldo}</h2>
+    <br>
+    </div>`
 
     let deposito = parseInt(formDeposito.value)
     console.log(deposito);
     if(!isNaN(deposito)){
-        usuario1.saldo+=deposito
+        usuarioOnline.saldo+=deposito
         resultadoOperaciones.className= "my-3 p-3 alert alert-primary";
-        resultadoOperaciones.innerHTML=`<p>Deposito exitoso. Tu nuevo saldo es $${usuario1.saldo}</p>` 
+        resultadoOperaciones.innerHTML=`<p>Deposito exitoso. Tu nuevo saldo es $${usuarioOnline.saldo}</p>` 
         let contenido = document.querySelector(".contenido").innerHTML=`<div class="contenido-active">
-        <h2>Bienvenido ${usuario1.nombre}</h2>
+        <h2>Bienvenido ${usuarioOnline.nombre}</h2>
         <br>
-        <h2>Tu saldo es de $${usuario1.saldo}</h2>
+        <h2>Tu saldo es de $${usuarioOnline.saldo}</h2>
         <br>
         </div>`  
         formDeposito.value=""  
@@ -109,20 +120,28 @@ function ingresarDinero(){
 
 
 function retirarDinero(){
+    let contenido = document.querySelector(".contenido").innerHTML=`<div class="contenido-active">
+    <h2>Bienvenido ${usuarioOnline.nombre}</h2>
+    <br>
+    <h2>Tu salgo es de $${usuarioOnline.saldo}</h2>
+    <br>
+    </div>`
+
+
     let retiro = parseInt(formExtraccion.value)
-    if(retiro >= usuario1.saldo || isNaN(retiro)){
+    if(retiro > usuarioOnline.saldo || isNaN(retiro)){
         resultadoOperaciones.className= "my-3 p-3  alert alert-danger";
         resultadoOperaciones.innerHTML=`<p>Saldo insuficiente o los datos ingresados son invalidos</p>`
         formExtraccion.value="" 
     }
     else{
-        usuario1.saldo-=retiro
+        usuarioOnline.saldo-=retiro
         resultadoOperaciones.className= "my-3 p-3 alert alert-primary";
-        resultadoOperaciones.innerHTML=`<p>Retiro exitoso. Tu nuevo saldo es $${usuario1.saldo}</p>` 
+        resultadoOperaciones.innerHTML=`<p>Retiro exitoso. Tu nuevo saldo es $${usuarioOnline.saldo}</p>` 
         let contenido = document.querySelector(".contenido").innerHTML=`<div class="contenido-active">
-        <h2>Bienvenido ${usuario1.nombre}</h2>
+        <h2>Bienvenido ${usuarioOnline.nombre}</h2>
         <br>
-        <h2>Tu salgo es de $${usuario1.saldo}</h2>
+        <h2>Tu salgo es de $${usuarioOnline.saldo}</h2>
         <br>
     </div>` 
         formExtraccion.value="" 
@@ -132,24 +151,22 @@ function retirarDinero(){
     
     
 function checkearDatosUsuario(){
-    fetch(`usuarios.json`)
-    .then(respuesta=>respuesta.json())
-    .then(data=>{
+    
         let contenido="";
-        for(const usuario of  data){
-        if(usuario.id==1){
+        
             contenido=`<div class="card" style="width: 18rem;">
             <ul class="list-group list-group-flush">
-            <li class="list-group-item">${usuario.nombre} ${usuario.apellido}</li>
-            <li class="list-group-item">DNI:${usuario.dni}</li>
-            <li class="list-group-item">Domicilio:${usuario.domicilio}</li>
-            <li class="list-group-item">Localidad:${usuario.localidad}</li>
-            <li class="list-group-item">Codigo Postal:${usuario.codigopostal}</li>
+            <li class="list-group-item">${usuarioOnline.nombre} ${usuarioOnline.apellido}</li>
+            <li class="list-group-item">DNI:${usuarioOnline.dni}</li>
+            <li class="list-group-item">Domicilio:${usuarioOnline.domicilio}</li>
+            <li class="list-group-item">Localidad:${usuarioOnline.localidad}</li>
+            <li class="list-group-item">Codigo Postal:${usuarioOnline.codigopostal}</li>
             </ul>
         </div>`
-    } 
-}   
 
-document.getElementById("contenido").innerHTML=contenido
-    })
-}
+        document.getElementById("contenido").innerHTML=contenido
+    } 
+
+    
+
+    
